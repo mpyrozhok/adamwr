ddbff72abca995666606a19ecc361193df9ca4b3
Maksym Pyrozhok <mpyrozhok@users.noreply.github.com>
2019-04-08 21:21:19 +0300

    Refactor policies into separate callable classes. Allow flexible max and min lr adjustment.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This repository contains an implementation of AdamW optimization algorithm and c
 Cosine annealing scheduler with restarts allows model to converge to a (possibly) different local minimum on every restart and normalizes weight decay hyperparameter value according to the length of restart period.
 Unlike schedulers presented in standard PyTorch scheduler suite this scheduler adjusts optimizer's learning rate not on every epoch, but on every batch update, according to the paper.
 Besides ["cosine"](https://www.google.com/search?q=(cos(x%2Fpi)%2B1)%2F2) there are two more learning rate policies available: ["arccosine"](https://www.google.com/search?q=arccos(2*x-1)%2Fpi), which has steeper profile at the limiting points and ["triangular"](https://www.google.com/search?q=1-abs(x*2-1)), which implements triangular lr policy proposed in https://arxiv.org/pdf/1506.01186v6.pdf.
-The ratio of increasing and decreasing phases for triangular policy could be adjusted with `triangular_step` parameter.
+The ratio of increasing and decreasing phases for triangular policy could be adjusted with `triangular_step` parameter. Minimum allowed lr is adjusted by `min_lr` parameter.
 This scheduler could be used with AdamW and other PyTorch optimizers.
 
 # Example:
--- cyclic_scheduler.py
+++ cyclic_scheduler.py
@@ -4,6 +4,44 @@ from torch.optim.lr_scheduler import _LRScheduler
 import math
 
 
+class ReduceMaxLROnRestart:
+    def __init__(self, ratio=0.75):
+        self.ratio = ratio
+
+    def __call__(self, eta_min, eta_max):
+        return eta_min, eta_max * self.ratio
+
+
+class ExpReduceMaxLROnIteration:
+    def __init__(self, gamma=1):
+        self.gamma = gamma
+
+    def __call__(self, eta_min, eta_max, iterations):
+        return eta_min, eta_max * self.gamma ** iterations
+
+
+class CosinePolicy:
+    def __call__(self, t_cur, restart_period):
+        return 0.5 * (1. + math.cos(math.pi *
+                                    (t_cur / restart_period)))
+
+
+class ArccosinePolicy:
+    def __call__(self, t_cur, restart_period):
+        return (math.acos(max(-1, min(1, 2 * t_cur
+                                      / restart_period - 1))) / math.pi)
+
+
+class TriangularPolicy:
+    def __call__(self, t_cur, restart_period):
+        inflection_point = 0.5 * restart_period
+        point_of_triangle = (t_cur / inflection_point
+                             if t_cur < inflection_point
+                             else 1.0 - (t_cur - inflection_point)
+                             / (restart_period - inflection_point))
+        return point_of_triangle
+
+
 class CyclicLRWithRestarts(_LRScheduler):
     """Decays learning rate with cosine annealing, normalizes weight decay
     hyperparameter value, implements restarts.
@@ -16,6 +54,7 @@ class CyclicLRWithRestarts(_LRScheduler):
         restart_period: epoch count in the first restart period
         t_mult: multiplication factor by which the next restart period will extend/shrink
         policy: ["cosine", "arccosine", "triangular"]
+        eta_policy: ["reduce_by_half", "exponential"]
 
 
     Example:
@@ -32,78 +71,90 @@ class CyclicLRWithRestarts(_LRScheduler):
     """
 
     def __init__(self, optimizer, batch_size, epoch_size, restart_period=100,
-                 t_mult=2, last_epoch=-1, eta_threshold=1000, verbose=False,
-                 policy="cosine", triangular_ratio=0.5):
+                 t_mult=2, last_epoch=-1, verbose=False,
+                 policy="cosine", policy_fn=None, min_lr=1e-8,
+                 eta_on_restart_cb=None, eta_on_iteration_cb=None):
+
         if not isinstance(optimizer, Optimizer):
             raise TypeError('{} is not an Optimizer'.format(
                 type(optimizer).__name__))
+
         self.optimizer = optimizer
+
         if last_epoch == -1:
             for group in optimizer.param_groups:
                 group.setdefault('initial_lr', group['lr'])
+                group.setdefault('minimum_lr', min_lr)
         else:
             for i, group in enumerate(optimizer.param_groups):
                 if 'initial_lr' not in group:
                     raise KeyError("param 'initial_lr' is not specified "
                                    "in param_groups[{}] when resuming an"
                                    " optimizer".format(i))
-        self.base_lrs = list(map(lambda group: group['initial_lr'],
-                                 optimizer.param_groups))
+
+        self.base_lrs = [group['initial_lr'] for group
+                         in optimizer.param_groups]
+
+        self.min_lrs = [group['minimum_lr'] for group
+                        in optimizer.param_groups]
+
+        self.base_weight_decays = [group['weight_decay'] for group
+                                   in optimizer.param_groups]
+
+        self.policy = policy
+        if policy_fn is not None:
+            self.policy_fn = policy_fn
+        elif self.policy == "cosine":
+            self.policy_fn = CosinePolicy()
+        elif self.policy == "arccosine":
+            self.policy_fn = ArccosinePolicy()
+        elif self.policy == "triangular":
+            self.policy_fn = TriangularPolicy()
 
         self.last_epoch = last_epoch
         self.batch_size = batch_size
-        self.iteration = 0
         self.epoch_size = epoch_size
-        self.eta_threshold = eta_threshold
+
+        self.iteration = 0
+        self.total_iterations = 0
+
         self.t_mult = t_mult
         self.verbose = verbose
-        self.base_weight_decays = list(map(lambda group: group['weight_decay'],
-                                           optimizer.param_groups))
         self.restart_period = math.ceil(restart_period)
         self.restarts = 0
         self.t_epoch = -1
-        self.policy = policy
-        self.triangular_ratio = triangular_ratio
+        self.epoch = -1
+
+        self.eta_min = 0
+        self.eta_max = 1
+        self.eta_on_restart_cb = eta_on_restart_cb
+        self.eta_on_iteration_cb = eta_on_iteration_cb
+
         self.end_of_period = False
         self.batch_increments = []
         self._set_batch_increment()
 
-    def _schedule_eta(self):
-        """
-        Threshold value could be adjusted to shrink eta_min and eta_max values.
-        """
-        eta_min = 0
-        eta_max = 1
-        if self.restarts <= self.eta_threshold:
-            return eta_min, eta_max
-        else:
-            d = self.restarts - self.eta_threshold
-            k = d * 0.05
-            return (eta_min + k, eta_max - k)
+    def _on_restart(self):
+        if self.eta_on_restart_cb is not None:
+            self.eta_min, self.eta_max = self.eta_on_restart_cb(self.eta_min,
+                                                                self.eta_max)
+
+    def _on_iteration(self):
+        if self.eta_on_iteration_cb is not None:
+            self.eta_min, self.eta_max = self.eta_on_iteration_cb(self.eta_min,
+                                                                  self.eta_max,
+                                                                  self.total_iterations)
 
     def get_lr(self, t_cur):
-        eta_min, eta_max = self._schedule_eta()
-
-        if self.policy == "cosine":
-            eta_t = (eta_min + 0.5 * (eta_max - eta_min)
-                     * (1. + math.cos(math.pi *
-                                      (t_cur / self.restart_period))))
-        elif self.policy == "arccosine":
-            eta_t = (eta_min + (eta_max - eta_min)
-                     * (math.acos(max(-1, min(1, 2 * t_cur /
-                                              self.restart_period - 1)))
-                        / math.pi))
-        elif self.policy == "triangular":
-            inflection_point = self.triangular_ratio * self.restart_period
-            eta_t = (t_cur / inflection_point if t_cur < inflection_point
-                     else 1.0 - (t_cur - inflection_point) / (self.restart_period - inflection_point))
-        else:
-            raise ValueError("Invalid LR policy specified.")
+        eta_t = (self.eta_min + (self.eta_max - self.eta_min)
+                 * self.policy_fn(t_cur, self.restart_period))
 
         weight_decay_norm_multi = math.sqrt(self.batch_size /
                                             (self.epoch_size *
                                              self.restart_period))
-        lrs = [base_lr * eta_t for base_lr in self.base_lrs]
+
+        lrs = [min_lr + (base_lr - min_lr) * eta_t for base_lr, min_lr
+               in zip(self.base_lrs, self.min_lrs)]
         weight_decays = [base_weight_decay * eta_t * weight_decay_norm_multi
                          for base_weight_decay in self.base_weight_decays]
 
@@ -112,10 +163,12 @@ class CyclicLRWithRestarts(_LRScheduler):
 
         if self.t_epoch % self.restart_period < self.t_epoch:
             if self.verbose:
-                print("Restart at epoch {}".format(self.last_epoch))
+                print("Restart {} at epoch {}".format(self.restarts + 1,
+                                                      self.last_epoch))
             self.restart_period = math.ceil(self.restart_period * self.t_mult)
             self.restarts += 1
             self.t_epoch = 0
+            self._on_restart()
             self.end_of_period = False
 
         return zip(lrs, weight_decays)
@@ -135,7 +188,9 @@ class CyclicLRWithRestarts(_LRScheduler):
     def batch_step(self):
         try:
             t_cur = self.t_epoch + self.batch_increments[self.iteration]
+            self._on_iteration()
             self.iteration += 1
+            self.total_iterations += 1
         except (IndexError):
             raise StopIteration("Epoch size and batch size used in the "
                                 "training loop and while initializing "
